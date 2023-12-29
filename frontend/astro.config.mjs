95bda3a847ed2c9b3a4a15020dfb9f6a7820c46f
apple-phi <lkn849@gmail.com>
2023-12-29 17:06:27 +0000

    FRONTEND: Add Svelte to Astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import node from "@astrojs/node";
-
+import svelte from "@astrojs/svelte";
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress()],
+  integrations: [svelte(), tailwind(), compress()],
   output: "hybrid",
   adapter: node({
-    mode: "standalone"
-  })
+    mode: "standalone",
+  }),
 });
